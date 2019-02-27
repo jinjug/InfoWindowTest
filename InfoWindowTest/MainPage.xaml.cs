7df2aa317c8226e1b68fc9193840ded0e7585cb2
jinjug <jinjug@fullscale.io>
2019-02-27 09:24:28 +0800

    - Initial Commit

---
+++ InfoWindowTest/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InfoWindowTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            map.Pins.Add(new Xamarin.Forms.GoogleMaps.Pin
+            {
+                Label= "ATM",
+                Address = "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum test test test test test test test test test.",
+                Position = new Xamarin.Forms.GoogleMaps.Position(0, 0)
+            });
+        }
+    }
+}
